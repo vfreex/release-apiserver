01b6ca312990a5693fbe97e5a57535ae06d3b08c
Yuxiang Zhu <yuxzhu@redhat.com>
2020-03-16 14:29:25 +0800

    First commit

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,50 @@
+
+/*
+Copyright 2020 The OCP Release APIServer Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package main
+
+import (
+	// Make sure dep tools picks up these dependencies
+	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "github.com/go-openapi/loads"
+
+	"sigs.k8s.io/apiserver-builder-alpha/pkg/cmd/server"
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // Enable cloud provider auth
+
+	"github.com/vfreex/release-apiserver/pkg/apis"
+	"github.com/vfreex/release-apiserver/pkg/openapi"
+	_ "github.com/vfreex/release-apiserver/plugin/admission/install"
+)
+
+func main() {
+	version := "v0"
+
+	err := server.StartApiServerWithOptions(&server.StartOptions{
+		EtcdPath:         "/registry/art.openshift.io",
+		Apis:             apis.GetAllApiBuilders(),
+		Openapidefs:      openapi.GetOpenAPIDefinitions,
+		Title:            "Api",
+		Version:          version,
+
+		// TweakConfigFuncs []func(apiServer *apiserver.Config) error
+		// FlagConfigFuncs []func(*cobra.Command) error
+	})
+	if err != nil {
+		panic(err)
+	}
+}
